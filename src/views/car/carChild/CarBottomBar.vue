606838bac57831c63fbc98596b84b94ae97c67d2
coder <25143078@qq.com>
2020-08-02 22:50:48 +0800

    完成购物车模块

---
+++ src/views/car/carChild/CarBottomBar.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "CarBottomBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/car/carChild/CarNavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "CarNavBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
